4825b3f7d2e659f98ccfc2d259efb10216afab7e
Galip Erdem <gterdem@sakarya.edu.tr>
2019-04-21 01:41:10 +0300

    Add project files.

---
+++ TaskRequest.Application/Class1.cs
@@ -0,0 +1,8 @@
+﻿using System;
+
+namespace TaskRequest.Application
+{
+    public class Class1
+    {
+    }
+}
---
+++ TaskRequest.Persistence/Config/ServerConfig.cs
@@ -0,0 +1,9 @@
+﻿using TaskRequest.Config;
+
+namespace TaskRequest.Persistence.Config
+{
+    public class ServerConfig
+    {
+        public MongoDBConfig MongoDB { get; set; } = new MongoDBConfig();
+    }
+}
